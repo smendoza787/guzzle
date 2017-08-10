54c0d908dabd64df930ede4966be8a9d5115f743
Sergio Mendoza <smendoza787@gmail.com>
2017-08-09 23:11:27 -0700

    add bars#create method

--- app/controllers/api/bars_controller.rb
+++ app/controllers/api/bars_controller.rb
@@ -3,4 +3,18 @@ class Api::BarsController < ApplicationController
     @bars = Bar.all
     render json: @bars
   end
+
+  def create
+    @bar = Bar.new(bar_params)
+    if @bar.save
+      render json: @bar
+    else
+      render json: { errors: {message: "This Bar Failed To Save" }}
+    end
+  end
+
+  private
+    def bar_params
+      params.require(:bar).permit(:name, :address, :city, :state, :rating)
+    end
 end
