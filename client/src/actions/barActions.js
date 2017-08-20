9c9c51d844c869c3beba8cdfe754434955c25d04
Sergio Mendoza <smendoza787@gmail.com>
2017-08-19 23:00:56 -0700

    changed fetch request in barActions to request from proxy

--- client/src/actions/barActions.js
+++ client/src/actions/barActions.js
@@ -1,5 +1,7 @@
 import fetch from 'isomorphic-fetch'
 
+const API_URL = process.env.REACT_APP_API_URL
+
 export function fetchBars() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_BARS' })
@@ -39,7 +41,7 @@ export function removeFavoriteBar(bar) {
 export function fetchPhoto() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_PHOTO' })
-    return fetch(`${API_URL}/unsplash`)
+    return fetch('/api/unsplash')
       .then(response => response.json())
       .then(photo => dispatch({ type: 'FETCH_PHOTO', payload: photo }))
   }
