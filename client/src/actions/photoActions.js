86e2d7180b93f68c617f60eb3cc955037459c758
Sergio Mendoza <smendoza787@gmail.com>
2017-08-19 23:47:04 -0700

    changed fetch requests to point to localhost:3001

--- client/src/actions/photoActions.js
+++ client/src/actions/photoActions.js
@@ -1,7 +1,7 @@
 export function fetchPhoto() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_PHOTO' })
-    return fetch('/api/unsplash')
+    return fetch('http://localhost:3000/api/unsplash')
       .then(response => response.json())
       .then(photo => dispatch({ type: 'FETCH_PHOTO', payload: photo }))
   }
