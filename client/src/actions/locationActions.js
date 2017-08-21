a16925737c5af6aeea3fbf683079d9a2c5ce880c
Sergio Mendoza <smendoza787@gmail.com>
2017-08-20 23:47:57 -0700

    removed env variable from fetch request and switched to proxy url

--- client/src/actions/locationActions.js
+++ client/src/actions/locationActions.js
@@ -7,7 +7,7 @@ export function fetchPlaces(latitude, longitude) {
 
   return (dispatch) => {
     dispatch({ type: 'LOADING_PLACES' })
-    return fetch(`${API_URL}/google_places`, {
+    return fetch('/api/google_places', {
       method: "POST",
       body: formData
     }).then(response => response.json())
