3b8cf595d533b7ee8411a8b0f5a63d059263cd0e
Sergio Mendoza <smendoza787@gmail.com>
2017-11-12 11:55:58 -0800

    refactored BarsShow and BarDetails because the previous way did not make sense

--- client/src/components/BarDetails.js
+++ client/src/components/BarDetails.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import FavoriteButton from './FavoriteButton'
-import Comments from '../containers/Comments'
 
-const BarDetails = ({ photo, bar, renderStars, comments, matchUrl }) => {
+const BarDetails = ({ bar, renderStars, matchUrl }) => {
   return (
-    <div className="bar-show">
-      <div className="bar-show-top-row">
-        <img className="bar-photo" src={photo.photo.url} alt="unsplash photography" />
-        <div className="bar-details">
-          <h1>{bar.name}</h1>
-          <h2>{renderStars(bar.rating)}</h2>
-          <h4>{bar.vicinity}</h4>
-          <FavoriteButton bar={bar} />
-        </div>
-      </div>
-      <Comments barComments={comments} match={matchUrl} />
+    <div className="bar-details">
+      <h1>{bar.name}</h1>
+      <h2>{renderStars(bar.rating)}</h2>
+      <h4>{bar.vicinity}</h4>
+      <FavoriteButton bar={bar} />
     </div>
   )
 }
--- client/src/containers/BarsShow.js
+++ client/src/containers/BarsShow.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux'
 import BarDetails from '../components/BarDetails'
 import { fetchPhoto } from '../actions/photoActions'
 import FontAwesome from 'react-fontawesome'
+import Comments from './Comments'
 
 class BarsShow extends Component {
   componentDidMount() {
@@ -31,13 +32,13 @@ class BarsShow extends Component {
 
   render() {
     return (
-      <BarDetails
-        photo={this.props.photo}
-        bar={this.props.bar}
-        renderStars={this.renderStars}
-        comments={this.props.comments}
-        matchUrl={this.props.match}
-      />
+      <div className="bar-show">
+        <img className="bar-photo" src={this.props.photo.photo.url} alt="unsplash photography" />
+        <BarDetails
+          bar={this.props.bar}
+          renderStars={this.renderStars} />
+        <Comments barComments={this.props.comments} match={this.props.match} />
+      </div>
     )
   }
 }
