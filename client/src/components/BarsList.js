4d49c75ac7114de44e0074e7d555f1a0068067f6
Sergio Mendoza <smendoza787@gmail.com>
2017-11-19 09:38:15 -0800

    move api fetch call from app component to barslist component

--- client/src/components/BarsList.js
+++ client/src/components/BarsList.js
@@ -1,50 +1,86 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import MenuItem from 'material-ui/MenuItem'
 import FontAwesome from 'react-fontawesome'
-import Loader from 'halogen/ScaleLoader';
+import Loader from 'halogen/ScaleLoader'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { fetchBars, fetchFavoriteBars } from '../actions/barActions'
+import { fetchPlaces } from '../actions/locationActions'
 
-const BarsList = ({ bars, favoriteBars }) => {
-  let renderBars, nonFavBars
+class BarsList extends Component {
+  componentWillMount() {
+    this.setLocation()
+    this.props.fetchBars()
+    this.props.fetchFavoriteBars()
+  }
 
-
-  if (bars.length > 0) {
-    nonFavBars = bars.filter(bar => {
-      for (var i = 0; i < favoriteBars.length; i++) {
-        if (bar.name === favoriteBars[i].name) {
-          return false
-        }
-      }
-      return true
+  setLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.props.fetchPlaces(position.coords.latitude, position.coords.longitude)
     })
-
-    nonFavBars = nonFavBars.map(bar =>
-      <MenuItem>
-        <Link key={bar.name} to={`/bars/${bar.place_id}`}>{bar.name}</Link>
-      </MenuItem>
-    )
-
-    favoriteBars = favoriteBars.map(bar =>
-      <MenuItem>
-        <FontAwesome name='star' /> <Link key={bar.name} to={`/bars/${bar.place_id}`}>{bar.name}</Link>
-      </MenuItem>
-    )
-
-    renderBars = favoriteBars.concat(nonFavBars)
-  } else {
-    renderBars = <Loader className="spinner" color="#ffffff" size="16px" margin="4px"/>
   }
 
 
+  render() {
+    let renderBars, renderFavoriteBars
 
-  return (
-    <div className="sidebar">
-      <h3 style={{ color: '#b1b1b1' }}>
-        Your Local Bars <FontAwesome name='glass' />
-      </h3>
-      {renderBars}
-    </div>
-  )
+    if (this.props.favoriteBars.length > 0) {
+      renderFavoriteBars = this.props.favoriteBars.map(bar =>
+        <MenuItem>
+          <FontAwesome name='star' /> <Link key={bar.name} to={`/bars/${bar.place_id}`}>{bar.name}</Link>
+        </MenuItem>
+      )
+    }
+
+    if (this.props.fetchedBars.length > 0) {
+      renderBars = this.props.fetchedBars.map(bar =>
+        <MenuItem>
+          <Link key={bar.name} to={`/bars/${bar.place_id}`}>{bar.name}</Link>
+        </MenuItem>
+      )
+    } else {
+      renderBars = <Loader className="spinner" color="#ffffff" size="16px" margin="4px"/>
+    }
+
+
+
+    return (
+      <div className="sidebar">
+        <h3 style={{ color: '#b1b1b1' }}>
+          Your Local Bars <FontAwesome name='glass' />
+        </h3>
+        {renderFavoriteBars}
+        {renderBars}
+      </div>
+    )
+  }
 }
 
-export default BarsList
+const mapStateToProps = (state) => {
+  // filter out favorite bars from fetched bars
+  let favNames = state.bars.favoriteBars.map(bar => bar.name)
+
+  let filterFetchedBars = state.bars.fetchedBars.filter(bar => {
+    if (favNames.includes(bar.name)) {
+      return false
+    } else {
+      return true
+    }
+  })
+
+  return {
+    fetchedBars: filterFetchedBars,
+    favoriteBars: state.bars.favoriteBars
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchBars,
+    fetchPlaces,
+    fetchFavoriteBars
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BarsList)
