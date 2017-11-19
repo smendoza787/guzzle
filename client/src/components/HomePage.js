97f9064e245f08f71df099c0225e9af49aadc9e1
Sergio Mendoza <smendoza787@gmail.com>
2017-11-19 09:56:06 -0800

    change homepage tagline

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -6,7 +6,7 @@ const Home = () => {
 	return (
 		<div className="home-page">
 	  	<h1><strong className="main-header">Thirsty? <FontAwesome name="beer"/></strong></h1>
-			<h2 className="main-header"><strong>I sure hope so.</strong></h2>
+			<h2 className="main-header"><strong>Search for your next favorite watering hole.</strong></h2>
 			<SearchFormContainer />
 		</div>
 	)
