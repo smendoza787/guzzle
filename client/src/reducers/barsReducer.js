f1da988074c1e8a994d802a34bab77bbefece235
Sergio Mendoza <smendoza787@gmail.com>
2017-11-19 09:40:10 -0800

    change bars state object shape

--- client/src/reducers/barsReducer.js
+++ client/src/reducers/barsReducer.js
@@ -1,12 +1,12 @@
-export default (state = { bars: [], favoriteBars: []}, action) => {
+export default (state = { fetchedBars: [], favoriteBars: []}, action) => {
   switch (action.type) {
     case 'FETCH_BARS':
-      return Object.assign({}, state, { bars: state.favoriteBars.concat(action.payload) })
+      return Object.assign({}, state, { fetchedBars: state.favoriteBars.concat(action.payload) })
     case 'FETCH_FAVORITE_BARS':
       return Object.assign({}, state, { favoriteBars: action.payload })
     case 'ADD_BAR':
       const bar = Object.assign({}, action.payload, { id: state.length + 1 })
-      return state.bars.concat(bar)
+      return state.fetchedBars.concat(bar)
     case 'ADD_FAVORITE_BAR':
       return Object.assign({}, state, { favoriteBars: state.favoriteBars.concat(action.payload) })
     case 'REMOVE_FAVORITE_BAR':
