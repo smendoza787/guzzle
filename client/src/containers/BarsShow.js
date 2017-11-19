02e099a4b0931f2418e2ff388776f519e4a4a810
Sergio Mendoza <smendoza787@gmail.com>
2017-11-19 09:39:51 -0800

    In BarsShow, if unsplash photo does not load, show the rest of the data

--- client/src/containers/BarsShow.js
+++ client/src/containers/BarsShow.js
@@ -8,7 +8,7 @@ import Comments from './Comments'
 import BarMap from './BarMap'
 
 class BarsShow extends Component {
-  componentDidMount() {
+  componentWillMount() {
     this.props.fetchPhoto()
   }
 
@@ -18,7 +18,7 @@ class BarsShow extends Component {
     }
   }
 
-  renderStars = (rating) => {
+  renderStars(rating) {
     let numOfStars = parseInt(rating)
     if (numOfStars > 0) {
       let faIcons = []
@@ -32,11 +32,9 @@ class BarsShow extends Component {
   }
 
   render() {
-    return (
-      <div className="bar-show">
-        <div className="bar-photo">
-          <img src={this.props.photo.photo.url} alt="unsplash photography" />
-        </div>
+    if (!this.props.photo) {
+      return (
+        <div className="bar-show">
         <BarDetails
           bar={this.props.bar}
           renderStars={this.renderStars} />
@@ -49,12 +47,32 @@ class BarsShow extends Component {
           isMarkerShown />
         <Comments barComments={this.props.comments} bar={this.props.bar} match={this.props.match} />
       </div>
-    )
+      )
+    } else {
+      return (
+        <div className="bar-show">
+          <div className="bar-photo">
+            <img src={this.props.photo.photo.url} alt="unsplash photography" />
+          </div>
+          <BarDetails
+            bar={this.props.bar}
+            renderStars={this.renderStars} />
+          <BarMap
+            coordinates={{ lat: this.props.bar.lat, lng: this.props.bar.lng }} 
+            containerElement={<div className="google-map" style={{ height: `450px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPoE2MRiJslI7MdbnsW32xvgCIHvLssBg&v=3.exp&libraries=geometry,drawing,places"          
+            loadingElement={<div style={{ height: `100%` }} />}
+            isMarkerShown />
+          <Comments barComments={this.props.comments} bar={this.props.bar} match={this.props.match} />
+        </div>
+      )
+    }
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const bar = state.bars.bars.find(bar => bar.id === ownProps.match.params.barId)
+  const bar = state.bars.fetchedBars.find(bar => bar.id === ownProps.match.params.barId)
   const comments = state.comments.filter(comment => comment.barId === ownProps.match.params.barId)
 
   if (bar) {
