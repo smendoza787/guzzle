19212a8a576f1554d8e56a06524e65ee6c5c7036
Sergio Mendoza <smendoza787@gmail.com>
2017-11-24 23:48:21 -0800

    changed aboutpage content

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -7,7 +7,10 @@ const AboutPage = () =>
     <h4>
       Guzzle is intended to be a one-stop shop for bartenders and service professionals, complete with 
       drink recipes, a tip calculator, tip tracker, and being able to search for bars within your area. 
-      Guzzle was previously named DrinksUp and only provided local bar information.
+      Guzzle was previously named DrinksUp and only provided local bar information using the Google Places API.
+      <br/><br/>
+      But as a developer, I believe an application geared towards bartenders and cocktail enthusiasts should be more than a Yelp clone, 
+      and more of a tool for anything they could possibly need when entertaining guests or having a moment to themselves to enjoy a nice beverage.
     </h4>
     <div className="about-links">
       <a href="https://github.com/smendoza787/guzzle"><FontAwesome name="github-square" /> Github Repo</a>
