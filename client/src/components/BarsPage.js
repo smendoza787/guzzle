4a7a2cb9eefb59051a292e2a14c3d2a3c67b00cb
Sergio Mendoza <smendoza787@gmail.com>
2017-11-28 03:37:58 -0800

    remove searchbar from bars page

--- client/src/components/BarsPage.js
+++ client/src/components/BarsPage.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import BarsShow from '../containers/BarsShow'
 import BarsList from './BarsList'
-import SearchFormContainer from '../containers/SearchFormContainer'
 
 const BarsPage = ({ match }) =>
-  <div>
+  <div class="bars=page">
     <BarsList />
-    <SearchFormContainer />
     <Switch>
       <Route path={`${match.url}/:barId`} component={BarsShow} />
     </Switch>
