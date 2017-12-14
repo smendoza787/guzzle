ef937ea22e8ed16c12e510220a469c1a288e8ab7
Sergio Mendoza <smendoza787@gmail.com>
2017-12-13 23:28:04 -0800

    setup API call to cocktailDB in RecipesPage

--- client/src/containers/RecipesPage.js
+++ client/src/containers/RecipesPage.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 
 class RecipesPage extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      recipes: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
+      .then(resp => resp.json())
+      .then(cocktails => this.setState({ recipes: cocktails }))
+  }
+
+  renderCocktails(recipes) {
+    return recipes.map(recipe =>
+      <div className="drink">
+        <h2>{recipe.strDrink}</h2>
+        <img src={recipe.strDrinkThumb} alt="drink thumbnail" height="150" width="150" />      
+      </div>
+    )
+  }
+
   render() {
+    console.log("RECIPEZ", this.state.recipes);
     return (
-      <h1>Hello From RecipesPage</h1>
+      <div>
+        <h1>Cocktail Recipes</h1>
+        <div className="recipe-list">
+          {this.state.recipes.drinks && this.renderCocktails(this.state.recipes.drinks)}
+        </div>
+      </div>
     )
   }
 }
