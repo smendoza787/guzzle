780fb119439b8b62f1be9b9eddcf149e1d6d7bcc
Sergio Mendoza <smendoza787@gmail.com>
2018-01-01 20:06:30 -0800

    create RecipeList component

---
+++ client/src/components/RecipeList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RecipeList extends Component {
+  render() {
+    return (
+      <h1>RecipeList Base: {this.props.base}</h1>
+    )
+  }
+}
+
+export default RecipeList
